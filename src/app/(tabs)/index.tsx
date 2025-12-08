7c5bbc181aff38fdcca696d9cad1bf73e052454b
stizzngwako7 <stizzngwako7@gmail.com>
2025-12-08 03:17:42 -0900

    merge master into main

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,38 @@
-import {View, Text} from 'react-native';
+import {View, FlatList,Dimensions, ViewToken } from 'react-native';
+import PostListItem from '@/components/PostListItem';
+import posts from '@assets/data/posts.json';
+import { useRef, useState } from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function HomeScreen() {
+  const { height } = Dimensions.get('window');
+  const [currentIndex, setCuttentIndex] = useState(0);
+
+  const onViewableChanged = useRef{({ viewableItems } : { viewableItems: viewToken[]}) => {
+    if (viewableItems.length > 0) {
+      setCuttentIndex(viewableItems[0]?.index || 0)
+    }
+  }}
+
+  console.log(currentidex)
   return (
     <View>
-      <Text>hellow world</Text>
+
+      <View>
+        <MaterialIcons name="live-tv" size=(24) color="white"/>
+      </View>
+  
+      <FlatList
+      data={posts}
+      renderItem= {({ item, index }) => {
+        <PostListItem postItem={item} isActive={index === currentIndex} />
+      }}
+      showsVerticalScrollIndicator={false}
+      snapToInterval={height - 80}
+      decelerationRate={"fast"}
+      disableIntervalMomentum
+      onViewableItemsChanged={onViawableItemChanged.current}
+      />
     </View>
   )
 }
